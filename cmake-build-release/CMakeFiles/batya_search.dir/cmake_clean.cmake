63e9b32f6b235b2506c0e34b64e501efc9ae5089
gnom6584 <gnom6584@gmail.com>
2021-07-11 21:15:56 +0300

    Init

---
+++ cmake-build-release/CMakeFiles/batya_search.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/batya_search.dir/main.cpp.o"
+  "batya_search"
+  "batya_search.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/batya_search.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+
+#include <filesystem>
+#include <regex>
+
+constexpr const auto HELP_FLAG = "-h";
+
+constexpr const auto DIRECTORY_FLAG = "-d";
+
+constexpr const auto RECURSIVE_FLAG = "-r";
+
+constexpr const auto HELP_MESSAGE =
+	"\nbatya_search [-flags] . [regex] - search in current directory recursively.\n"
+	"\nbatya_search [-flags] [directory path] [regex] - search in directory.\n"
+	"\nFlags:\n"
+	"\n\t-i - search indexed.\n"
+	"\n\t-r - search recursively.\n";
+
+
+using namespace std;
+
+inline void process_directory_entry(const filesystem::directory_entry& dir_entry, const regex& regex, size_t& count, bool indexed) {
+	try {
+		const auto dir_path = dir_entry.path().native();
+		if (regex_match(dir_path, regex)) {
+			if (indexed)
+				cout << "[" << count << "] ";
+			cout << dir_path << endl;
+			++count;
+		}
+	}
+	catch (exception& exception) {
+		cerr << exception.what() << endl << endl;
+	}
+}
+
+int main(int argc, char* argv[]) {
+
+	bool indexed = false;
+	bool recursive = false;
+
+	if(argc <= 2) {
+		cout << HELP_MESSAGE;
+		exit(1);
+	}
+
+	if(argc > 3)
+		for(size_t i = argc - 3; i > 0; --i) {
+			if (strcmp(argv[i], "-i") == 0)
+				indexed = true;
+			else if (strcmp(argv[i], "-r") == 0)
+				recursive = true;
+			else {
+				cerr << "Invalid flag: " << argv[i] << endl;
+				exit(1);
+			}
+		}
+
+	auto arg_directory = argv[argc - 2];
+	auto arg_regex = argv[argc - 1];
+
+	string directory = arg_directory[0] == '.'? filesystem::current_path().string() : arg_directory;
+
+	if(not filesystem::is_directory(directory)) {
+		cout << directory << " is not directory!" << endl;
+		exit(1);
+	}
+
+
+	cout << endl << "Search in directory = " << directory << ": " << endl << endl;
+
+	regex regex = std::regex(arg_regex);
+
+	size_t count {};
+
+	try {
+		if (recursive)
+			for (const auto& dir_entry : filesystem::recursive_directory_iterator(directory))
+				process_directory_entry(dir_entry, regex, count, indexed);
+		else
+			for (const auto& dir_entry : filesystem::directory_iterator(directory))
+				process_directory_entry(dir_entry, regex, count, indexed);
+	}
+	catch (exception& exception) {
+		cerr << exception.what() << endl << endl;
+	}
+	if(count == 0)
+		cout << "No files found!" << endl;
+	else
+		cout << endl << count << " files found!" << endl;
+	return 0;
+}
